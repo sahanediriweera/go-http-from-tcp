b11bc9f328213886d02599ef991b9a6577a3daac
sahanediriweera <rsahanediriweera@gmail.com>
2026-01-10 22:35:45 +0530

    refactor using channels

--- main.go
+++ main.go
@@ -3,9 +3,44 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			fmt.Printf("read: %s\n", str)
+		}
+	}()
+
+	return out
+}
 
 func main() {
 
@@ -14,27 +49,10 @@ func main() {
 		log.Fatal("error", "error", err)
 	}
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
+	lines := getLinesChannel(f)
 
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s \n", str)
-			str = ""
-		}
-
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 
 }
